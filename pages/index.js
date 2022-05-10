941a4bde5359de620876f867636b1c5d8f1c609a
london-newyork <cactus1272004@yahoo.co.jp>
2022-05-10 15:01:58 +0900

    Tailwind css環境設定

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React, { useCallback, useState, useEffect } from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(props) {
+  const [posts, setPosts] = useState([])
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const json = await res.json()
+    setPosts(json);
+  }, [])
+
+  useEffect(() => {
+    getPosts()
+  }, [getPosts])
+
+
   return (
     <div>
       <Head>
@@ -10,9 +24,14 @@ export default function Home() {
         <meta name="description" content="Practice API" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-
+      <main className='px-10 mt-10'>
+        <ol className='list-decimal'>
+          {posts.map((post) => {
+            return (
+              <li key={post.id}>{post.title}</li>
+            )
+          })}
+        </ol>
       </main>
     </div>
   )
